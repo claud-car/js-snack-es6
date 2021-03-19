942b645fe1a4524926b06488543aae90c4ef4f67
Claudio <claudiocarchietti@yahoo.it>
2021-03-19 18:25:10 +0100

    Esercizio completato

--- JSsnack3/main.js
+++ JSsnack3/main.js
@@ -52,7 +52,7 @@ gatti.forEach((item) => {
 
 console.log(gatti);
 
-// MILESTONE 2
+// MILESTONE 2 e 3
 
 
 //ciclo for-each dell'array per determinare il colore dei fiocchi e sesso
@@ -73,7 +73,7 @@ gatti.forEach((item) => {
     colore = 'rosachiaro'
   }
   //stampo in html con le classi corrispondenti scritte in css
-  $('#gatti2').append(`<li class="${colore}">${item.nome} è ${item.sesso}<i class="fas fa-ribbon"></i></li><br>`)
+  $('#gatti2').append(`<li class="${colore}">${item.nome} è ${item.sesso} <i class="fas fa-ribbon"></i></li><br>`)
 });
 
 //divido in 2 array per poi unire e ordinare i 2 array
@@ -85,6 +85,99 @@ console.log(maschi);
 
 let gattiordinati = [...femmine,...maschi];
 console.log(gattiordinati);
+
+const maschiopacity = [];
+
+maschi.forEach((item) => {
+  let opacita = "";
+  if (item.eta >= 10) {
+    opacita = 1;
+  } else {
+    opacita = 0.5;
+  }
+
+  var item = {...item,opacity: opacita};
+
+  maschiopacity.push(maschi)
+
+    $('#gatti3').append(`<li class="azzurro">${item.nome} , gatto di colore ${item.colore}, <i style = "opacity:${opacita};" class="fas fa-ribbon"></i></li><br>`)
+})
+
+const femmineopacity = [];
+
+femmine.forEach((item) => {
+  let opacita = "";
+  if (item.eta >= 10) {
+    opacita = 1;
+  } else {
+    opacita = 0.5;
+  }
+
+  var item = {...item,opacity: opacita};
+
+  femmineopacity.push(femmine)
+
+  $('#gatti3').append(`<li class="rosa">${item.nome} , gatto di colore ${item.colore}, <i style = "opacity:${opacita};" class="fas fa-ribbon"></i></li><br>`)
+})
+
+
+
+
+
+// const gattibastardi = {...gattiordinati,opacita: 1};
+// console.log(gattibastardi);
+
+
+
+// gattiordinati.forEach((item) => {
+//   const gattinuovi = gattiordinati.map((element, i, array) => {
+//     if (item.eta > 10) {
+//       let obj = {
+//         ...element,
+//         opacita: 1
+//       }
+//       return obj;
+//     } else {
+//       let obj = {
+//         ...element,
+//         opacita: 0.5
+//       }
+//       return obj;
+//     }
+//   });
+//   $('#gatti3').append(`<li>${item.nome} è ${item.sesso},con opacità ${item.opacita} <i class="fas fa-ribbon"></i></li><br>`)
+// });
+
+
+
+
+
+// gattiordinati.forEach((item) => {
+//   let colore = [];
+//   //condizione per cambiare colore dei fiocchi
+//   if (item.sesso == "Maschio" && item.eta > 10) {
+//     //se è maschio ed ha più di 10 anni
+//     colore = 'azzurro';
+//   } else if (item.sesso == "Maschio" && item.eta < 10){
+//     //se è maschio ed ha meno di 10 anni
+//     colore = 'azzurrochiaro';
+//   } else if (item.sesso == "Femmina" && item.eta >10){
+//     //se è femmina ed ha più di 10 anni
+//     colore = 'rosa';
+//   } else if (item.sesso == "Femmina" && item.eta <10){
+//     //se è femmina ed ha meno di 10 anni
+//     colore = 'rosachiaro'
+//   };
+//
+//   if (item.eta > 10) {
+//     $('#gatti3 > li > i').css("opacity",0.5);
+//     console.log();
+//   } else {
+//     $('#gatti3 > li > i').css("opacity",1);
+//   }
+//   $('#gatti3').append(`<li class="${colore}">${item.nome} è ${item.sesso} <i class="fas fa-ribbon"></i></li><br>`)
+
+
 
 
 
