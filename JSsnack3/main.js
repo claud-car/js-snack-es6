ad86399d9d313179438a5daccf541cadac46a419
Claudio <claudiocarchietti@yahoo.it>
2021-03-19 18:28:33 +0100

    Esercizio terminato

--- JSsnack3/main.js
+++ JSsnack3/main.js
@@ -150,8 +150,6 @@ femmine.forEach((item) => {
 
 
 
-
-
 // gattiordinati.forEach((item) => {
 //   let colore = [];
 //   //condizione per cambiare colore dei fiocchi
@@ -178,9 +176,6 @@ femmine.forEach((item) => {
 //   $('#gatti3').append(`<li class="${colore}">${item.nome} è ${item.sesso} <i class="fas fa-ribbon"></i></li><br>`)
 
 
-
-
-
 // gatti.forEach((item) => {
 //
 //
@@ -204,72 +199,6 @@ femmine.forEach((item) => {
 //   console.log(item);
 // });
 
-
-
 //MILESTONE 3
 
 //creo 2 array per i gatti maschi e femmine
-
-
-
-
-///////////////////////////////
-
-
-
-// const studenti = ['Dora', 'Andrea', 'Andrea', 'Giuseppe'];
-//
-// studenti.forEach((elemento, index, array) => {
-//   console.log(elemento, array);
-// });
-//
-// const students = [
-//   {
-//     name: 'Dora',
-//     gender: 'F',
-//   },
-//   {
-//     name: 'Andrea',
-//     gender: 'M',
-//   },
-//   {
-//     name: 'Giuditta',
-//     gender: 'F',
-//   }
-// ];
-//
-// students.forEach((item) => {
-//   console.log(`${item.name} è ${item.gender}`);
-// });
-//
-//
-// const newStudenti = studenti.map((element, i, array) => {
-//   if (i%2) {
-//     return '';
-//   }
-//
-//   return element;
-// });
-//
-// console.log(newStudenti);
-//
-// const newStudents = students.map((element, i, array) => {
-//   console.log(i);
-//   let obj = {
-//     ...element,
-//     iscritto: true
-//   }
-//   return obj;
-// });
-//
-// students.map((element, i, array) => {
-//   console.log(students);
-//   let obj = {
-//     ...element,
-//     iscritto: true
-//   }
-//
-//   array[i] = obj;
-// });
-//
-// console.log(students);
